5028f4de35600075cb89084d8732e7507e14594b
Peter Cotton <57455669+microprediction@users.noreply.github.com>
2021-12-27 19:19:21 -0500

    Update test_internet.py

--- tests/test_internet.py
+++ tests/test_internet.py
@@ -3,6 +3,6 @@ from microprediction import MicroReader
 
 def test_imports():
     mr = MicroReader()
-    values = mr.get_lagged_values(name='cop.json')
+    values = mr.get_lagged_values(name='three_body_x.json')
     assert len(values)>500
 
